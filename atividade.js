5007974625acd5eb2fd15946bf6a68f43ef34a83
Fabiola <fabiola.goms770@gmail.com>
2021-09-17 22:17:41 -0300

    subindo atividade

---
+++ atividade.js
@@ -0,0 +1,17 @@
+var quantidadedealunos = 12
+
+for (let index = 0; index <= quantidadedealunos ;index++){
+const sala = quantidadedealunos[index]
+
+
+    if (index == 0 ){
+        console.log (" número é zero "+ index);
+    }
+    else if ((index % 2) == 0){
+        console.log("o número é par " + index )
+    }
+    else {
+        console.log("o número é impar " + index)
+}
+}
+
